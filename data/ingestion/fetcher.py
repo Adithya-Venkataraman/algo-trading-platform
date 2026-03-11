4eb95a56acec1b8ee027e6ed3a58776ce80a0808
Adithya-Venkataraman <lazerwing9@gmail.com>
2026-03-11 18:52:05 +0000

    Week 1 :multi ticker ingestion+error handling

--- data/ingestion/fetcher.py
+++ data/ingestion/fetcher.py
@@ -4,15 +4,18 @@ from data.db_connection import get_connection
 import yfinance as yf
 conn=get_connection()
 cursor = conn.cursor()
-tickers=["AAPL"]
+tickers=["AAPL","GOOG","MSFT","AMZN","BTC-USD"]
 
 for ticker in tickers:
-    t=yf.Ticker(ticker)
-    df=t.history(period="1y")
-    for index,row in df.iterrows():
-        cursor.execute("INSERT INTO stock_prices(time,symbol,open,high,low,close,volume) values(%s,%s,%s,%s,%s,%s,%s)",
-    (index, ticker, float(row['Open']), float(row['High']), float(row['Low']), float(row['Close']), int(row['Volume'])))
-    conn.commit()
-print("Data inserted Successfully")
+    try:
+        t=yf.Ticker(ticker)
+        df=t.history(period="1y")
+        for index,row in df.iterrows():
+            cursor.execute("INSERT INTO stock_prices(time,symbol,open,high,low,close,volume) values(%s,%s,%s,%s,%s,%s,%s)",
+            (index, ticker, float(row['Open']), float(row['High']), float(row['Low']), float(row['Close']), int(row['Volume'])))
+        conn.commit()
+        print(f"Data for {ticker} inserted successfully")
+    except Exception as e:
+        print(f"Error inserting data for {ticker}: {e}")
 conn.close()
     
